f8ab8592006ea2a84cfd9ea5db4bc92b902e3f23
ahmedhany1284 <ahmedhany4900@gmail.com>
2023-08-20 00:40:34 +0300

    final commit: completing the design and maping the tones and the colrs together

--- lib/layout/tunes_screen.dart
+++ lib/layout/tunes_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tunes_app/models/tune_model.dart';
 import 'package:tunes_app/widgets/tune_item.dart';
 
 class TunesScreen extends StatelessWidget {
-  const TunesScreen({Key? key}) : super(key: key);
-  final List<Color> tuneColor= const[
-    Color.fromRGBO(236, 43, 59, 1),
-    Color.fromRGBO(243, 148, 49, 1),
-    Color.fromRGBO(250, 243, 49, 1),
-    Color.fromRGBO(61, 194, 90, 1),
-    Color.fromRGBO(13, 166, 134, 1),
-    Color.fromRGBO(13, 161, 231, 1),
-    Color.fromRGBO(153, 17, 166, 1),
+   const TunesScreen({Key? key}) : super(key: key);
+  final List<TuneModel> tunes= const[
+    TuneModel(color: Color.fromRGBO(236, 43, 59, 1),sound:'note1.wav' ),
+    TuneModel(color: Color.fromRGBO(243, 148, 49, 1),sound:'note2.wav' ),
+    TuneModel(color: Color.fromRGBO(250, 243, 49, 1),sound:'note3.wav' ),
+    TuneModel(color: Color.fromRGBO(61, 194, 90, 1),sound:'note4.wav' ),
+    TuneModel(color:Color.fromRGBO(13, 166, 134, 1),sound:'note5.wav' ),
+    TuneModel(color:  Color.fromRGBO(13, 161, 231, 1),sound:'note6.wav' ),
+    TuneModel(color: Color.fromRGBO(153, 17, 166, 1),sound:'note7.wav' ),
+
 
   ];
   @override
@@ -23,15 +25,8 @@ class TunesScreen extends StatelessWidget {
         elevation: 0.0 ,
       ),
       body: Column(
-        children: getTuneItems(),
+        children: tunes.map((e) => TuneItem(tune: e,)).toList(),
       ),
     );
-  }
-  List<TuneItem> getTuneItems(){
-    List<TuneItem> items =[];
-    for(var color in tuneColor){
-      items.add(TuneItem(color: color));
-    }
-    return items;
   }
 }
--- lib/widgets/tune_item.dart
+++ lib/widgets/tune_item.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:tunes_app/models/tune_model.dart';
 
 class TuneItem extends StatelessWidget {
-  const TuneItem({Key? key, required this.color}) : super(key: key);
-  final Color color;
+  const TuneItem({Key? key, required this.tune}) : super(key: key);
+  final TuneModel tune;
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        color: color,
+      child: GestureDetector(
+        onTap: (){
+          tune.playSound();
+        },
+        child: Container(
+          color: tune.color,
+        ),
       ),
     );
   }
